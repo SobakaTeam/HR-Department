dc646bb8d230f48739d1cba680a748d760a3a826
Илья Цихо <piioogjfjvj1@outlook.com>
2025-01-21 21:39:20 +0300

    не Готово

---
+++ HR-Department.DeskTop/Connection.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace HR_Department.DeskTop
+{
+    internal class Connection
+    {
+        HttpClient client = new HttpClient();
+        Client.BaseAddress = new Uri("http://example.com/");
+    }
+}
--- HR-Department.DeskTop/Form1.cs
+++ HR-Department.DeskTop/Form1.cs
@@ -1,3 +1,6 @@
+using System.Data.SqlClient;
+using HR_Department.DeskTop;
+using System.Data.SqlClient;
 namespace HR_Department.DeskTop
 {
     public partial class Form1 : Form
@@ -5,6 +8,11 @@ namespace HR_Department.DeskTop
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
